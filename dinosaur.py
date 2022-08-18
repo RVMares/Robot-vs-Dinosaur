dbddf72d74a2f6afe11e1b68e3fd31f89a5a2c7b
Ruth Hermosillo <hermosilloruth59@gmail.com>
2022-08-17 19:30:52 -0600

    Created the Robot Class

--- dinosaur.py
+++ dinosaur.py
@@ -2,6 +2,7 @@ class Dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
         self.attack_power = attack_power
+        self.health = int
     
     def attack(self, robot):
         pass
---
+++ robot.py
@@ -0,0 +1,10 @@
+from email.base64mime import header_length
+from weapon import Weapon
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = int
+        self.active_weapon = Weapon
+    def attack (self, dinosaur):
+        pass
